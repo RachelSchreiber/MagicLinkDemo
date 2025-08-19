088352c7fe628e527cd3f77a0aa73ca055c8c1f9
RachelSchreiber <r0583214262@gmail.com>
2025-08-20 01:37:34 +0300

    Update email subject for consistency with application branding

--- Services/EmailSender.cs
+++ Services/EmailSender.cs
@@ -18,7 +18,7 @@ public class EmailSender
 
     public async Task SendMagicLinkAsync(string toEmail, string magicLink)
     {
-        var subject = "Your Secure Login Link - YourAppName";
+        var subject = "Your Secure Login Link - MagicLinkDemo";
         var htmlBody = $@"
 <!DOCTYPE html>
 <html>
